ebcc9b5de372023d5e15aa316ddbc019518f6df3
Vincent Bayer <vinci.bayer@gmx.de>
2019-11-12 18:38:18 +0100

    Zahlungsscreen eingefügt

---
+++ src/page-payment/page-payment.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/**
+*   Klasse PageLogin:Ermöglicht dem Benutzer das Anmelden
+**/
+class PagePayment {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+        let counter = 0;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show(matches) {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("page-payment/page-payment.html");
+        let css = await fetch("page-payment/page-payment.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+
+        pageDom.querySelector("#next").addEventListener("click", () => this.    switchpage());
+
+        this._app.setPageTitle("R&C - Zahlvorgang");
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+
+    switchpage(){
+        alert("Hilfe");
+    }
+}
